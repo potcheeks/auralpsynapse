df0e862745982dc0c144a1706e0080e8cc410a35
Charmaine Chan <chachan@Charmaines-MacBook-Pro.local>
2021-07-02 09:46:55 +0800

    testing deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import "./App.css";
 import Dropdown from "./components/Dropdown";
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function App() {
-
-  
-const [artist, setArtist] = useState("Armin Van Buuren");
+  const [artist, setArtist] = useState("Armin Van Buuren");
 
   const dropdownData = [
     { value: 1, name: "Mood" },
     { value: 2, name: "Generating Playlist..." },
     { value: 3, name: "Visual Orgasm" },
   ];
-  
-  // 
+
+  //
   //
 
   //youtube video: http://theaudiodb.com/api/v1/json/${process.env.REACT_APP_APIKEY}/mvid.php?i=112024
-  
+
   useEffect(() => {
-    const artistUrl= `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
-    
+    const artistUrl = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+
     const makeApiCall = () => {
       fetch(artistUrl)
         .then((res) => res.json())
@@ -31,13 +29,13 @@ const [artist, setArtist] = useState("Armin Van Buuren");
     };
     makeApiCall();
   }, []);
-  
-  console.log(artist)
+
 
   return (
     <form onSubmit={() => {}}>
       <div className="container">
-        <Dropdown options={dropdownData} />
+        <input type="text" 
+        Classname="typebox" />
         {artist}
         <Dropdown options={dropdownData} />
         <button type="submit">Beam me up Scottaye!</button>
